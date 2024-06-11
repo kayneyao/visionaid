b16e024d93f26e396a87dd24196ecf80f4d0e20a
kayneyao <42885964+kayneyao@users.noreply.github.com>
2024-06-11 11:21:00 +0800

    Complete Instructions

---
+++ gps/geopy.py
@@ -0,0 +1,12 @@
+import geopy.geocoders as gc
+
+# agent = gc.Nominatim(user_agent="Geopy Library")
+
+# address = input()
+
+# location = agent.geocode(address)
+
+# print(location.address)
+# print(location.latitude)
+# print(location.longitude)
+# print(location.raw)
